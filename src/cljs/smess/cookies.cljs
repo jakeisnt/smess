d9659958f1f886f1e18f9bff0839e2becfa40985
Jacob Chvatal <jake@isnt.online>
2021-03-19 15:09:27 -0700

    Add non-mutating cookie function 2

--- src/cljs/smess/cookies.cljs
+++ src/cljs/smess/cookies.cljs
@@ -1,18 +1,23 @@
 (ns smess.cookies)
 
 (defn cookie->clj
+  "Convert a cookie string to a clojure map."
+  [cookiestr]
+    ;; creates {:key value :key2 value2}
+  (apply merge
+           ;; creates ({:key value} {:key value})
+         (map #(hash-map (keyword (first %1)) (str (second %1)))
+         ;; generates ((key, value), (key, value)) etc...
+              (map
+               (fn [str] (.split str "="))
+               (.split cookiestr ";")))))
+
+(defn cookie->clj!
   "Gets the browser cookie as a clojure map into memory."
   []
   (let
-   [cookie (.-cookie js/document)]
-    ;; creates {:key value :key2 value2}
-    (apply merge
-           ;; creates ({:key value} {:key value})
-           (map #(hash-map (keyword (first %1)) (str (second %1)))
-         ;; generates ((key, value), (key, value)) etc...
-                (map
-                 (fn [str] (.split str "="))
-                 (.split cookie ";"))))))
+   [cookiestr (.-cookie js/document)]
+    (cookie->clj cookiestr)))
 
 (defn clj->cookie
   "Convert a clojure map to a cookie"
