27267c17e3010c94910b466a18071690926f5052
Jacob Chvatal <jake@isnt.online>
2021-03-20 01:10:16 -0700

    fix key chaos

--- src/cljs/smess/chat/history.cljs
+++ src/cljs/smess/chat/history.cljs
@@ -45,6 +45,7 @@
                                 :list (cons
                                        {:user (:user msg)
                                         :id (:id msg)
+                                        :key (:id msg)
                                         :messages (list msg)}
                                        last-list)}]
                    ret-obj))))
@@ -56,20 +57,24 @@
   "A single message."
   [m selected-message]
   (reagent/create-class
-   {:render (fn [] [:div {:key (str "msg-" (:id m))
-                          :id (str "msg-" (:id m))
-                          :class "message"
-                          :style {:background-color (if (= (:id m) (:id @selected-message)) "azure" nil)}}
-                    (if (:reply-to m) (message-reply (:reply-to m)) nil)
-                    [:div {:class "message-content"}
-                     (markdown-preview (:msg m))
-                     [:div {:class "message-buttons"}
-                      [:button {:key (str (:id m) "-text-button")
-                                :class "text-button"
-                                :onClick (fn [] (to-clipboard (:msg m)))}
-                       "copy text"]
-                      [:button  "copy link"]
-                      [:button {:onClick (fn [] (reset! selected-message m))} "reply"]]]])}))
+   {:render (fn []
+              [:div {:key (str "msg-" (:id m))
+                     :id (str "msg-" (:id m))
+                     :class "message"
+                     :style {:background-color (if (= (:id m) (:id @selected-message)) "azure" nil)}}
+               (if (:reply-to m) (message-reply (:reply-to m)) nil)
+               [:div {:class "message-content"
+                      :key (str "message-content" (:id m))}
+                (markdown-preview (:msg m))
+                [:div {:class "message-buttons"
+                       :key (str "message-buttons" (:id m))}
+                 [:button {:key (str (:id m) "-text-button")
+                           :class "text-button"
+                           :onClick (fn [] (to-clipboard (:msg m)))}
+                  "copy text"]
+                 [:button {:key (str (:id m) "-link-button")} "copy link"]
+                 [:button {:key (str (:id m) "-reply-button")
+                           :onClick (fn [] (reset! selected-message m))} "reply"]]]])}))
 
 (defn chat-history
   "Display the history of the chat."
@@ -81,7 +86,10 @@
                         [:div {:key (str (:user usermsg) "-" (:id usermsg))
                                :class "usermsg"}
                          (username-box (:user usermsg) app-state)
-                         (for [m (:messages usermsg)] [message m selected-message])]))])
+                         (for [m (:messages usermsg)]
+
+                           ^{:key (str "msg-" (:id m))}
+                           [message m selected-message])]))])
 
     :component-did-update (fn [this]
                             (let [node (reagent/dom-node this)]
