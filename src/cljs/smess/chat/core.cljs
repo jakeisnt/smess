b7bfe3063a5c13c78118a24954cdc28cf707f183
Jacob Chvatal <jake@isnt.online>
2022-05-20 09:28:04 -0400

    make a core item a constant

--- src/cljs/smess/chat/core.cljs
+++ src/cljs/smess/chat/core.cljs
@@ -8,12 +8,14 @@
 ;; the currently selected message to reply to
 (defonce reply-to-message (atom nil))
 
+;; the title of the app
+(defonce title [:div {:class "header"} [:h3 "SMESS"]])
+
 (defn chat-view
   "Displays all of the chat history."
   [app-state msg-list users]
   [:div {:class "chat-container"}
-   [:div {:class "header"}
-    [:h3 "smess"]]
+   title
    [sidebar users app-state]
    [chat-history msg-list app-state reply-to-message]
    [chat-input app-state reply-to-message]])
